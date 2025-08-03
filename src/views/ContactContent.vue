b9054d8015b4937c55af22c251028aff06eacae9
Gustav Hersmann <gustav@gustavhersmann.com>
2025-08-03 17:18:50 +0200

    Refactor: modify MainCircle to render Home-Contact views

---
+++ src/views/ContactContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>{{ t('contact1') }}</p>
+    <p>{{ t('contact2') }}</p>
+    <p>{{ t('contact3') }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
